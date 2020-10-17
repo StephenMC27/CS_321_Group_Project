3b226422e3a1cb90466b2ec31ee67a4d08fc1049
Stephen Clark <stephenmclark527@gmail.com>
2020-10-17 16:54:15 -0400

    Added constants to hold API keys

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import tweepy
 
+#API credentials for application
+CONSUMER_KEY = 'FLIiklJ1Ju7WiS5FmzAqtytNZ'
+CONSUMER_SECRET = 'ZD6jyBaH0BvqdnnKwu1ssrnEwAwOcNFxcGGLEXC9jCDv4I209Z'
+
+#API credentials for associating application with @GoodMorningGMU account
+ACCESS_KEY = '1310571338936049664-G76N1UbCtuLbn20tvGjjOos8GiDy8f'
+ACCESS_SECRET = 'bYq5eHFZ2MdiKo4Cq5qXeDNFfmZCaCP7I7zCjSNTMPEip'
+
