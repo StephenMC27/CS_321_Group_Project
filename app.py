a886c0a28027eca00549a509ec7efd92bb6e4616
Stephen Clark <stephenmclark527@gmail.com>
2020-10-17 17:26:28 -0400

    Authorize bot to publish tweets

--- app.py
+++ app.py
@@ -8,3 +8,11 @@ CONSUMER_SECRET = 'ZD6jyBaH0BvqdnnKwu1ssrnEwAwOcNFxcGGLEXC9jCDv4I209Z'
 ACCESS_KEY = '1310571338936049664-G76N1UbCtuLbn20tvGjjOos8GiDy8f'
 ACCESS_SECRET = 'bYq5eHFZ2MdiKo4Cq5qXeDNFfmZCaCP7I7zCjSNTMPEip'
 
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+api = tweepy.API(auth)
+
+#api.update_status('Testing, testing, 1, 2, 3...')
