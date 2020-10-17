087bb04af6612e07d6c4385378b30109d2e382fd
Stephen Clark <stephenmclark527@gmail.com>
2020-10-17 17:43:41 -0400

    Test commit 2

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 import tweepy
 
-#API credentials for application
-CONSUMER_KEY = 'FLIiklJ1Ju7WiS5FmzAqtytNZ'
-CONSUMER_SECRET = 'ZD6jyBaH0BvqdnnKwu1ssrnEwAwOcNFxcGGLEXC9jCDv4I209Z'
-
-#API credentials for associating application with @GoodMorningGMU account
-ACCESS_KEY = '1310571338936049664-G76N1UbCtuLbn20tvGjjOos8GiDy8f'
-ACCESS_SECRET = 'bYq5eHFZ2MdiKo4Cq5qXeDNFfmZCaCP7I7zCjSNTMPEip'
-
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -16,3 +8,5 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 #api.update_status('Testing, testing, 1, 2, 3...')
+
+#test commit 2
