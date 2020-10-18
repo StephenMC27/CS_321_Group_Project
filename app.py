388d5fc41a4b188bd01ec7870c0063164eac8f51
Stephen Clark <stephenmclark527@gmail.com>
2020-10-17 21:44:13 -0400

    Create .gitignore for configuration file

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import tweepy
+import yaml
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+#read in API keys from config.yaml
+with open('config.yaml', r) as config_file:
+    config = yaml.load(config_file)
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+#save API keys
+CONSUMER_KEY = config['twitter']['consumer_key']
+CONSUMER_SECRET = config['twitter']['consumer_secret']
+ACCESS_KEY = config['twitter']['access_key']
+ACCESS_SECRET = config['twitter']['access_secret']
 
-api = tweepy.API(auth)
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET) #established bot's access to Twitter API
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET) #associates bot with specific Twitter account
+
+api = tweepy.API(auth) #object for making API requests
 
 #api.update_status('Testing, testing, 1, 2, 3...')
-
-#test commit 2
