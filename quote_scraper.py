8cb290c4d4ad43a42fc97c86d54c6d7a4e8007f4
Stephen Clark <stephenmclark527@gmail.com>
2020-10-27 16:54:46 -0400

    Parsed quote website HTML to find relevant elements

--- quote_scraper.py
+++ quote_scraper.py
@@ -1,3 +1,13 @@
 from requests_html import HTMLSession
 
 session = HTMLSession()
+
+response = session.get('https://www.goodreads.com/quotes/tag/inspirational')
+
+quote_elements = response.html.find('.quote.mediumText ')
+
+# for quote_element in quote_elements:
+#     quote_string = quote_element.find('.quoteText')[0].text
+#     print(quote_string)
+
+print(quote_elements[0].find('.quoteText')[0].html)
