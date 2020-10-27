3f5b6c858c56dc6785403b34532bd150c72925e0
Stephen Clark <stephenmclark527@gmail.com>
2020-10-27 14:25:22 -0400

    Added pseudocode for Tweet creation/publishing

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import tweepy
 import yaml
+from tweet import Tweet
+from bot import Bot
 
 #read in API keys from config.yaml
 with open('config.yaml', r) as config_file:
@@ -11,5 +13,9 @@ CONSUMER_SECRET = config['twitter']['consumer_secret']
 ACCESS_KEY = config['twitter']['access_key']
 ACCESS_SECRET = config['twitter']['access_secret']
 
-
-#api.update_status('Testing, testing, 1, 2, 3...')
+#get weather stuff
+#get song rec. stuff
+#get quote stuff
+#get image stuff
+#create new Tweet instance with above info
+#call Bot.publish_tweet(), passing in new tweet string
