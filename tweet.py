431d8ef80ac0bf564f979968b64e850b927d6f9e
Stephen Clark <stephenmclark527@gmail.com>
2020-10-27 13:38:57 -0400

    Created Tweet class

---
+++ tweet.py
@@ -0,0 +1,11 @@
+class Tweet:
+
+    def __init__(self, weather_str, song_str, quote_str, image):
+        self.weather_str = weather_str
+        self.song_str = song_str
+        self.quote_str = quote_str
+        self.image = image
+
+    def format(): -> str
+
+        #organize strings into tweet string here
