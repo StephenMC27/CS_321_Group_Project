8705aadad898fcc70e2c003c7ee20caad6c087ef
Stephen Clark <stephenmclark527@gmail.com>
2020-10-27 13:41:52 -0400

    Created Bot class

---
+++ bot.py
@@ -0,0 +1,9 @@
+class Bot:
+
+    def set_api_keys(cls, consumer_key, consumer_secret, access_key, access_secret):
+        cls.consumer_key = consumer_key
+        cls.consumer_secret = consumer_secret
+        cls.access_key = access_key
+        cls.access_secret = access_secret
+
+    
