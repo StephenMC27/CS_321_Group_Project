87acb21dd21a933e06ac15bf4bd1e1ff409a6db3
Stephen Clark <stephenmclark527@gmail.com>
2020-10-27 14:21:03 -0400

    Reorganized Bot.authorize()

--- bot.py
+++ bot.py
@@ -9,8 +9,8 @@ class Bot:
     def authorize(cls, consumer_key, consumer_secret, access_key, access_secret):
         auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET) #established bot's access to Twitter API
         auth.set_access_token(ACCESS_KEY, ACCESS_SECRET) #associate bot with specific Twitter account
+        api = tweepy.API(auth) #object for making API requests
 
     #publish Tweet
     def publish_tweet(cls, tweet_str):
-        api = tweepy.API(auth) #object for making API requests
         api.update_status(tweet_str)
