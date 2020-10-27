828e6a06021891a1347a1209e0712ce96fba8177
Stephen Clark <stephenmclark527@gmail.com>
2020-10-27 14:01:27 -0400

    Removed code to establish API access (included in bot.py)

--- app.py
+++ app.py
@@ -11,9 +11,4 @@ CONSUMER_SECRET = config['twitter']['consumer_secret']
 ACCESS_KEY = config['twitter']['access_key']
 ACCESS_SECRET = config['twitter']['access_secret']
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET) #established bot's access to Twitter API
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET) #associate bot with specific Twitter account
-
-api = tweepy.API(auth) #object for making API requests
-
 #api.update_status('Testing, testing, 1, 2, 3...')
