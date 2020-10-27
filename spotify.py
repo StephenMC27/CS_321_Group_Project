79c42481db17f2ffad00c4ac25cacc8b17699451
joshharrin <53531213+joshharrin@users.noreply.github.com>
2020-10-27 12:22:36 -0400

    Update spotify.py
    testing search for various artists

--- spotify.py
+++ spotify.py
@@ -13,6 +13,6 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET))
 
-results = sp.search(q='weezer', limit=20)
+results = sp.search(q='maren morris', limit=20)
 for idx, track in enumerate(results['tracks']['items']):
     print(idx, track['name'])
