312f445a76e559533d4459c7e9572860ddbcc874
joshharrin <53531213+joshharrin@users.noreply.github.com>
2020-10-29 18:00:46 -0400

    Update spotify.py
    adding functionality for new release, successfully grabbing some information from new songs

--- spotify.py
+++ spotify.py
@@ -13,6 +13,28 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET))
 
-results = sp.search(q='maren morris', limit=20)
+
+
+results = sp.search(q='maren morris', limit=3)
+print(results)
 for idx, track in enumerate(results['tracks']['items']):
     print(idx, track['name'])
+
+
+# consider calling this with a random value for offset to provide random selection from the weekly created playlist
+
+result1 = sp.playlist_items(playlist_id='37i9dQZF1DX4JAvHpjipBk', limit=5, additional_types={'track'})
+print(result1)
+print()
+for idx1, track1 in enumerate(result1['items']):
+	trackInfo = track1['track']
+	print("name: " + trackInfo['name'])
+	print("href: " + trackInfo['href'])
+	print("id: " + trackInfo['id'])
+	print("external url: " + trackInfo['external_urls']['spotify'])
+	print()
+	
+# maybe construct an array to store the songs for the week or something of that sort?
+
+
+
