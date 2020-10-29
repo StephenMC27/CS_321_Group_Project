ee5b508af46f165b0f5664b2d21480c5bf469f90
joshharrin <53531213+joshharrin@users.noreply.github.com>
2020-10-29 18:19:17 -0400

    Update spotify.py
    simplifies to print url for new music releases

--- spotify.py
+++ spotify.py
@@ -14,22 +14,23 @@ from spotipy.oauth2 import SpotifyClientCredentials
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET))
 
 
-
+"""
+example usage of search feature for spotify api: 
 results = sp.search(q='maren morris', limit=10)
 for idx, track in enumerate(results['tracks']['items']):
     print(idx, track['name'])
-
+"""
 
 # call with a random value for offset to provide random selection from the weekly created playlist
 
-result1 = sp.playlist_items(playlist_id='37i9dQZF1DX4JAvHpjipBk', offset=50, limit=5, additional_types={'track'})
+result1 = sp.playlist_items(playlist_id='37i9dQZF1DX4JAvHpjipBk', offset=0, limit=7, additional_types={'track'})
 print()
 for idx1, track1 in enumerate(result1['items']):
 	trackInfo = track1['track']
-	print("name: " + trackInfo['name'])
-	print("href: " + trackInfo['href'])
-	print("id: " + trackInfo['id'])
-	print("external url: " + trackInfo['external_urls']['spotify'])
+	# info: trackInfo['name']
+	# track id: trackInfo['id']
+	# spotify external url: trackInfo['external_urls']['spotify']
+	print("spotify url: " + trackInfo['external_urls']['spotify'])
 	print()
 	
 # maybe construct an array to store the songs for the week or something of that sort?
