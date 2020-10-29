957065ec9673ce8d2a48f192d9b6c776b24ac5c4
Stephen Clark <stephenmclark527@gmail.com>
2020-10-29 17:17:39 -0400

    Add basic methods

--- quote.py
+++ quote.py
@@ -1,3 +1,14 @@
+import csv
+
 class Quote:
 
-    #methods here
+    quotes = []
+
+    @classmethod
+    def fetch_quotes(csv_name):
+        #read quotes into list
+        csv_reader = csvreader()
+
+    @classmethod
+    def get_quote():
+        return(quotes.sample())
