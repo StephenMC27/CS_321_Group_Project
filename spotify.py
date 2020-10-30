2b8641c9e532292077ee4a05c45305f223b233b2
joshharrin <53531213+joshharrin@users.noreply.github.com>
2020-10-30 14:10:48 -0400

    Update spotify.py
    implemented random selection from weekly playlist

--- spotify.py
+++ spotify.py
@@ -1,5 +1,6 @@
 import spotipy
 import yaml
+from random import randint
 from spotipy.oauth2 import SpotifyClientCredentials
 
 #read in API keys from config.yaml
@@ -12,16 +13,19 @@ SPOTIFY_CLIENT_SECRET = config['spotify']['client_secret']
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET))
 
-# call with a random value for offset to provide random selection from the weekly created playlist, make sure offset is less than 100
+# generate random value for offset to provide random selection 
+rand_Offset = randint(0,80)
 
-result1 = sp.playlist_items(playlist_id='37i9dQZF1DX4JAvHpjipBk', offset=0, limit=7, additional_types={'track'})
-print()
+# call to grab items from weekly new release playlist, with random offset
+result1 = sp.playlist_items(playlist_id='37i9dQZF1DX4JAvHpjipBk', offset=rand_Offset, limit=7, additional_types={'track'})
+
+# print track url for each track desired
 for idx, track in enumerate(result1['items']):
 	trackInfo = track['track']
 	# info: trackInfo['name']
 	# track id: trackInfo['id']
 	# spotify external url: trackInfo['external_urls']['spotify']
-	print(idx, " spotify url: " + trackInfo['external_urls']['spotify'])
+	print(rand_Offset+idx, " spotify url: " + trackInfo['external_urls']['spotify'])
 	print()
 	
 # maybe construct an array to store the songs for the week or something of that sort?
