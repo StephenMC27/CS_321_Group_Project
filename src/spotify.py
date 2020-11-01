18421aeb725552cefb14d6d47a1bb8c3b8d29b5b
Stephen Clark <stephenmclark527@gmail.com>
2020-11-01 01:31:40 -0400

    Update path to config file in call to open

--- src/spotify.py
+++ src/spotify.py
@@ -4,7 +4,7 @@ from random import randint
 from spotipy.oauth2 import SpotifyClientCredentials
 
 #read in API keys from config.yaml
-with open('config.yaml', 'r') as config_file:
+with open('../config/config.yaml', 'r') as config_file:
 	config = yaml.load(config_file, Loader=yaml.FullLoader)
 
 #save API keys for spotify API
@@ -13,7 +13,7 @@ SPOTIFY_CLIENT_SECRET = config['spotify']['client_secret']
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET))
 
-# generate random value for offset to provide random selection 
+# generate random value for offset to provide random selection
 rand_Offset = randint(0,80)
 
 # call to grab items from weekly new release playlist, with random offset
@@ -27,12 +27,12 @@ for idx, track in enumerate(result1['items']):
 	# spotify external url: trackInfo['external_urls']['spotify']
 	print(rand_Offset+idx, " spotify url: " + trackInfo['external_urls']['spotify'])
 	print()
-	
+
 # maybe construct an array to store the songs for the week or something of that sort?
 
 
 """
-example usage of search feature for spotify api: 
+example usage of search feature for spotify api:
 results = sp.search(q='maren morris', limit=10)
 for idx, track in enumerate(results['tracks']['items']):
     print(idx, track['name'])
