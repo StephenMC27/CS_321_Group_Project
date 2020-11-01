180fe92b5451f6e3b9f3517b26179c847c60cb9c
Stephen Clark <stephenmclark527@gmail.com>
2020-11-01 14:57:25 -0500

    Fixed incorrect parameter in format()

--- src/tweet.py
+++ src/tweet.py
@@ -6,9 +6,8 @@ class Tweet:
         self.quote_str = quote_str
         self.image = image
 
-    def format(cls) -> str:
+    def format(self) -> str:
         tweet_str = 'Good Morning, GMU!\n'
         tweet_str += f'Current weather in Fairfax: {self.weather_str}\n'
         tweet_str += f'Listen to this: {self.song_str}\n'
         tweet_str += quoute_str
-        
