1e242b9b4364a947366e7b724b9a79610482d654
Stephen Clark <stephenmclark527@gmail.com>
2020-11-01 15:26:58 -0500

    Edited line spacing in format()

--- src/tweet.py
+++ src/tweet.py
@@ -7,8 +7,8 @@ class Tweet:
         self.image = None
 
     def format(self) -> str:
-        tweet_str = 'Good Morning, GMU!\n'
-        tweet_str += f'Current weather in Fairfax: {self.weather_str}\n'
-        tweet_str += f'Listen to this: {self.song_str}\n'
+        tweet_str = 'Good Morning, GMU!\n\n'
+        tweet_str += f'Current weather in Fairfax: {self.weather_str}\n\n'
+        tweet_str += f'Listen to this: {self.song_str}\n\n'
         tweet_str += self.quote_str
         return tweet_str
