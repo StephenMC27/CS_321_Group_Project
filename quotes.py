a53fdf6d1b4e6cb660078f4135c0888e5b79a438
Stephen Clark <stephenmclark527@gmail.com>
2020-11-01 01:19:38 -0400

    Implemented Quotes.fetch_quotes()

--- quotes.py
+++ quotes.py
@@ -1,14 +1,25 @@
 import csv
+import random
 
 class Quotes:
 
     quotes = []
 
     @classmethod
-    def fetch_quotes(csv_file):
-        #read quotes into list
-        csv_reader = csvreader()
+    def fetch_quotes(cls, csv_file):
+        #open csv file
+        with open(csv_file, newline='') as quotes_file:
+            #read quotes into list
+            quote_reader = csv.reader(quotes_file)
+            for row in quote_reader:
+                cls.quotes.append(row)
 
     @classmethod
-    def get_quote():
-        return(quotes.sample())
+    def get_quote(cls):
+        return(random.choice(cls.quotes))
+
+# if __name__ == '__main__':
+#     Quotes.fetch_quotes('quotes.csv')
+#     print(Quotes.quotes)
+#     random_quote = Quotes.get_quote()
+#     print(random_quote[0])
