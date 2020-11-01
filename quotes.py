475c65121d242bf14b49e60a8461202bd51564ce
Stephen Clark <stephenmclark527@gmail.com>
2020-11-01 01:05:18 -0400

    Changed class name to Quotes

---
+++ quotes.py
@@ -0,0 +1,14 @@
+import csv
+
+class Quotes:
+
+    quotes = []
+
+    @classmethod
+    def fetch_quotes(csv_file):
+        #read quotes into list
+        csv_reader = csvreader()
+
+    @classmethod
+    def get_quote():
+        return(quotes.sample())
