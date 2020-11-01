09cd8e9679d4426bfd2ba926b51ddacf4642c7cc
Stephen Clark <stephenmclark527@gmail.com>
2020-11-01 01:44:16 -0400

    Create run_bot() function

--- src/app.py
+++ src/app.py
@@ -8,20 +8,23 @@ from bot import Bot
 with open('../config/config.yaml', 'r') as config_file:
     config = yaml.load(config_file, yaml.FullLoader)
 
-#save API keys
+#save Twitter API keys
 CONSUMER_KEY = config['twitter']['consumer_key']
 CONSUMER_SECRET = config['twitter']['consumer_secret']
 ACCESS_KEY = config['twitter']['access_key']
 ACCESS_SECRET = config['twitter']['access_secret']
 
-#get weather stuff
-#get song rec. stuff
-quote = Quotes.get_quote()
-#get image stuff
-#create new Tweet instance with above info
-#call Bot.publish_tweet(), passing in new tweet string
+def gather_content():
 
-#test tweet
-tweet = 'Testing, testing, 1, 2, 3...'
-Bot.authorize(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
-Bot.publish_tweet(tweet)
+def run_bot():
+    #get weather stuff
+    #get song rec. stuff
+    quote = Quotes.get_quote()
+    #get image stuff
+    #create new Tweet instance with above info
+    #call Bot.publish_tweet(), passing in new tweet string
+
+    #test tweet
+    tweet = 'Testing, testing, 1, 2, 3...'
+    Bot.authorize(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+    Bot.publish_tweet(tweet)
