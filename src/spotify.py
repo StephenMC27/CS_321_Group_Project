dd5c3d07bbd0d414b0c729a7b362baa846333548
joshharrin <53531213+joshharrin@users.noreply.github.com>
2020-11-03 18:35:56 -0500

    Update spotify.py
    changed to class method

--- src/spotify.py
+++ src/spotify.py
@@ -3,19 +3,39 @@ import yaml
 from random import randint
 from spotipy.oauth2 import SpotifyClientCredentials
 
-#read in API keys from config.yaml
-with open('../config/config.yaml', 'r') as config_file:
-	config = yaml.load(config_file, Loader=yaml.FullLoader)
+class Spotify:
 
-#save API keys for spotify API
-SPOTIFY_CLIENT_ID = config['spotify']['client_id']
-SPOTIFY_CLIENT_SECRET = config['spotify']['client_secret']
+    weeklySongs = []
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET))
+    @classmethod
+    def fetch_songs(cls):
+        #read in API keys from config.yaml
+        with open('../config/config.yaml', 'r') as config_file:
+	        config = yaml.load(config_file, Loader=yaml.FullLoader)
+            
+        #save API keys for spotify API
+        SPOTIFY_CLIENT_ID = config['spotify']['client_id']
+        SPOTIFY_CLIENT_SECRET = config['spotify']['client_secret']
+        
+        sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET))
+        
+        # generate random value for offset to provide random selection
+        rand_Offset = randint(0,80)
+        
+        # call to grab items from weekly new release playlist, with random offset
+        result1 = sp.playlist_items(playlist_id='37i9dQZF1DX4JAvHpjipBk', offset=rand_Offset, limit=7, additional_types={'track'})
 
-# generate random value for offset to provide random selection
-rand_Offset = randint(0,80)
+        # print track url for each track desired
+        for idx, track in enumerate(result1['items']):
+            trackInfo = track['track']
+            cls.weeklySongs.append(trackInfo['external_urls']['spotify'])
 
+    @classmethod
+    def get_song(cls, day):
+        return(cls.weeklySongs[day])
+
+
+"""
 # call to grab items from weekly new release playlist, with random offset
 result1 = sp.playlist_items(playlist_id='37i9dQZF1DX4JAvHpjipBk', offset=rand_Offset, limit=7, additional_types={'track'})
 
@@ -26,14 +46,4 @@ for idx, track in enumerate(result1['items']):
 	# track id: trackInfo['id']
 	# spotify external url: trackInfo['external_urls']['spotify']
 	print(rand_Offset+idx, " spotify url: " + trackInfo['external_urls']['spotify'])
-	print()
-
-# maybe construct an array to store the songs for the week or something of that sort?
-
-
-"""
-example usage of search feature for spotify api:
-results = sp.search(q='maren morris', limit=10)
-for idx, track in enumerate(results['tracks']['items']):
-    print(idx, track['name'])
 """
