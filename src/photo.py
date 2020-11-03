2b14150112ef10ecd282684ce60a4f89888d177e
joshharrin <53531213+joshharrin@users.noreply.github.com>
2020-11-03 18:51:17 -0500

    Update photo.py
    updated photo size to have a higher resolution

--- src/photo.py
+++ src/photo.py
@@ -6,7 +6,7 @@ class Photo:
     def fetch_photo(cls):
 	    #fetch new image from lorem picsum
         filename = '../imagecache/todayspic.jpg'
-        request = requests.get("https://picsum.photos/400", stream=True)
+        request = requests.get("https://picsum.photos/800", stream=True)
         if request.status_code == 200:
             with open(filename, 'wb') as image:
                 for c in request:
