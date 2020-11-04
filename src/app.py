0516661ab94914ff8fbf1b2232344e5bd2aac962
Jake <51935523+jzegeer@users.noreply.github.com>
2020-11-04 08:21:54 -0500

    Updated weather content

--- src/app.py
+++ src/app.py
@@ -21,11 +21,10 @@ ACCESS_KEY = config['twitter']['access_key']
 ACCESS_SECRET = config['twitter']['access_secret']
 
 def create_tweet(): #returns a Tweet object
-    #precipObj = Weather() #into class
-    #OWM_API_KEY = precipObj.secure_key() #pulls from file(returns API KEY)
-    #j = precipObj.gather_info(OWM_API_KEY) #pulls dictionary for weather info from API
-    #weather_str = precipObj.get_values(j) #stores values into strings
-    weather_str = 'test'
+    precipObj = Weather() #into class
+    OWM_API_KEY = precipObj.secure_key() #pulls from file(returns API KEY)
+    j = precipObj.gather_info(OWM_API_KEY) #pulls dictionary for weather info from API
+    weather_str = precipObj.get_values(j) #stores values into strings
     Spotify.fetch_songs() # want to change this to only do this once a week
     song_str = Spotify.get_song(0) # instead of zero, pass in day of week
     Quotes.fetch_quotes('../csv/quotes.csv')
