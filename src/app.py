959a52f6163524fe2758084730447e0342a27f64
Jake <51935523+jzegeer@users.noreply.github.com>
2020-11-07 18:00:46 -0500

    Add files via upload
    Fixed weather calls

--- src/app.py
+++ src/app.py
@@ -9,7 +9,6 @@ from spotify import Spotify
 from weather import Weather
 
 MAX_TWEET_LENGTH = 280
-PHOTO_PATH = '../imagecache/todayspic.jpg'
 
 #read in API keys from config.yaml
 with open('../config/config.yaml', 'r') as config_file:
@@ -22,10 +21,8 @@ ACCESS_KEY = config['twitter']['access_key']
 ACCESS_SECRET = config['twitter']['access_secret']
 
 def create_tweet(): #returns a Tweet object
-    precipObj = Weather() #into class
-    OWM_API_KEY = precipObj.secure_key() #pulls from file(returns API KEY)
-    j = precipObj.gather_info(OWM_API_KEY) #pulls dictionary for weather info from API
-    weather_str = precipObj.get_values(j) #stores values into strings
+    Weather.gather_info()
+    weather_str = Weather.get_values(Weather.j)
     Spotify.fetch_songs() # want to change this to only do this once a week
     song_str = Spotify.get_song(0) # instead of zero, pass in day of week
     Quotes.fetch_quotes('../csv/quotes.csv')
@@ -39,14 +36,15 @@ def create_tweet(): #returns a Tweet object
             break
 
     #get image stuff
-    tweet = Tweet(weather_str, song_str, quote_str, PHOTO_PATH)
+    tweet = Tweet(weather_str, song_str, quote_str)
     return tweet
 
 def run_bot():
     #call Bot.publish_tweet(), passing in new tweet string
     tweet = create_tweet()
+    tweet_str = tweet.format()
     Bot.authorize(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
-    Bot.publish_tweet(tweet)
+    Bot.publish_tweet(tweet_str)
 
 schedule.every().day.at("08:05").do(run_bot)
 run_bot() #test
