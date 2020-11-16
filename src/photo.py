a82207b5b42f486c54740a5832cb480070e74ef6
joshharrin <53531213+joshharrin@users.noreply.github.com>
2020-11-16 14:49:01 -0500

    Update photo.py
    update photo.py to a module instead of class

--- src/photo.py
+++ src/photo.py
@@ -1,15 +1,12 @@
 import requests
 
-class Photo:
-
-    @classmethod
-    def fetch_photo(cls):
-	    #fetch new image from lorem picsum
-        filename = '../imagecache/todayspic.jpg'
-        request = requests.get("https://picsum.photos/800", stream=True)
-        if request.status_code == 200:
-            with open(filename, 'wb') as image:
-                for c in request:
-                    image.write(c)
-        else:
-            print("Unable to download image, using previous cached image")
+def fetch_photo(cls):
+    #fetch new image from lorem picsum
+    filename = '../imagecache/todayspic.jpg'
+    request = requests.get("https://picsum.photos/800", stream=True)
+    if request.status_code == 200:
+        with open(filename, 'wb') as image:
+            for c in request:
+                image.write(c)
+    else:
+        print("Unable to download image, using previous cached image")
